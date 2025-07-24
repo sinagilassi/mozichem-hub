a14711079e205d1f1c034551b4e60b350b4e9f71
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-07-24 16:13:25 -0400

    test examples

---
+++ examples/execute/bubble-temperature.py
@@ -0,0 +1,68 @@
+# import libs
+from mozichem_hub import (
+    __version__,
+)
+from mozichem_hub.executors import ToolExecuter
+from mozichem_hub.models import Pressure, Component
+from mozichem_hub.prebuilt import (
+    create_mozichem_mcp,
+    get_mozichem_mcp
+)
+# log
+from rich import print
+
+# NOTE: version
+print(f"[bold green]Mozichem Hub Version: {__version__}[/bold green]")
+
+# SECTION: mcp names
+mcp_names = get_mozichem_mcp()
+print(f"mcp names: {mcp_names}")
+
+# SECTION: Build the MCP server
+flash_calculations_mcp = create_mozichem_mcp(name="flash-calculations-mcp")
+
+# NOTE: mcp tools
+# tools_info = thermo_models_mcp.tools_info()
+# print(f"Tools available in 'thermo-models-mcp': {tools_info}")
+
+# SECTION: Create a ToolExecuter instance
+tool_executer = ToolExecuter(mozichem_mcp=flash_calculations_mcp)
+# all tools
+tools_ = tool_executer.get_tools()
+print(f"All tools: {tools_}")
+
+# select a tool to execute
+tool_name = "calc_bubble_temperature_ideal_vapor_ideal_liquid"
+
+# NOTE: arguments for the tool
+# pressure
+pressure = Pressure(
+    value=101.3,
+    unit="kPa"
+)
+
+# NOTE: components
+# benzene
+component_1 = Component(
+    name="benzene",
+    formula="C6H6",
+    state="l",
+    mole_fraction=0.26
+)
+# toluene
+component_2 = Component(
+    name="toluene",
+    formula="C7H8",
+    state="l",
+    mole_fraction=0.74
+)
+
+components = [component_1, component_2]
+
+# SECTION: Execute the tool
+result = tool_executer.execute_tool(
+    tool_name=tool_name,
+    components=components,
+    pressure=pressure,
+)
+print(f"Result of '{tool_name}': {result}")
