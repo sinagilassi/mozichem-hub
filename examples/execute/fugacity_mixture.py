f54082d9edd71fa98f53bd5e175a1e119275afd9
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-07-24 14:26:38 -0400

    move to examples

---
+++ examples/execute/fugacity_mixture.py
@@ -0,0 +1,75 @@
+# import libs
+from mozichem_hub import (
+    __version__
+)
+from mozichem_hub.prebuilt import (
+    create_mozichem_mcp,
+    get_mozichem_mcp
+)
+from mozichem_hub.executors import ToolExecuter
+from mozichem_hub.models import Temperature, Pressure, Component
+# log
+from rich import print
+
+# NOTE: version
+print(f"[bold green]Mozichem Hub Version: {__version__}[/bold green]")
+
+# SECTION: mcp names
+mcp_names = get_mozichem_mcp()
+print(f"mcp names: {mcp_names}")
+
+# SECTION: Build the MCP server
+thermo_models_mcp = create_mozichem_mcp(name="eos-models-mcp")
+
+# NOTE: mcp tools
+# tools_info = thermo_models_mcp.tools_info()
+# print(f"Tools available in 'thermo-models-mcp': {tools_info}")
+
+# SECTION: Create a ToolExecuter instance
+tool_executer = ToolExecuter(mozichem_mcp=thermo_models_mcp)
+# all tools
+tools_ = tool_executer.get_tools()
+print(f"All tools in 'eos-models-mcp': {tools_}")
+
+# select a tool to execute
+tool_name = "calc_fugacity_gas_mixture"
+
+# arguments for the tool
+temperature = Temperature(
+    value=298.15,
+    unit="K"
+)
+
+pressure = Pressure(
+    value=10,
+    unit="bar"
+)
+
+# methane
+component_1 = Component(
+    name="methane",
+    formula="CH4",
+    state="g",
+    mole_fraction=0.35
+)
+# ethane
+component_2 = Component(
+    name="ethane",
+    formula="C2H6",
+    state="g",
+    mole_fraction=0.65
+)
+
+components = [component_1, component_2]
+
+eos_model = "SRK"
+
+# SECTION: Execute the tool
+result = tool_executer.execute_tool(
+    tool_name=tool_name,
+    components=components,
+    temperature=temperature,
+    pressure=pressure,
+    eos_model=eos_model
+)
+print(f"Result of '{tool_name}': {result}")
---
+++ examples/references/reference-config.py
@@ -0,0 +1,70 @@
+# import libs
+from mozichem_hub.references import ReferencesAdapter
+from rich import print
+import pyThermoDB as ptdb
+
+# SECTION: check versions
+print(f"pyThermoDB version: {ptdb.__version__}")
+
+# NOTE:
+reference_config = {
+    "CO2":
+    {
+        'heat-capacity': {
+            'databook': 'CUSTOM-REF-1',
+            'table': 'Ideal-Gas-Molar-Heat-Capacity',
+        },
+        'vapor-pressure': {
+            'databook': 'CUSTOM-REF-1',
+            'table': 'Vapor-Pressure',
+        },
+        'general': {
+            'databook': 'CUSTOM-REF-1',
+            'table': 'General-Data',
+        },
+    }
+}
+
+reference_config_yml = """
+CO2:
+  heat-capacity:
+    databook: CUSTOM-REF-1
+    table: Ideal-Gas-Molar-Heat-Capacity
+    label: Cp_IG
+  vapor-pressure:
+    databook: CUSTOM-REF-1
+    table: Vapor-Pressure
+    label: VaPr
+  general:
+    databook: CUSTOM-REF-1
+    table: General-Data
+    labels:
+      Pc: Pc
+      Tc: Tc
+      AcFa: AcFa
+CO:
+  heat-capacity:
+    databook: CUSTOM-REF-1
+    table: Ideal-Gas-Molar-Heat-Capacity
+    label: Cp_IG
+  vapor-pressure:
+    databook: CUSTOM-REF-1
+    table: Vapor-Pressure
+    label: VaPr
+  general:
+    databook: CUSTOM-REF-1
+    table: General-Data
+    labels:
+      Pc: Pc
+      Tc: Tc
+      AcFa: AcFa
+"""
+
+# SECTION: Create a ReferencesAdapter instance
+ref_adapter = ReferencesAdapter()
+
+config_ = ref_adapter.config_from_str(
+    reference_config=reference_config_yml,
+)
+print(config_)
+print(type(config_))
