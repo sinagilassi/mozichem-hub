b28f52eb4deda72b2b3da0f3d2e5df2920c4aa77
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-07-23 21:20:05 -0400

    add conversion method for numeric strings

--- mozichem_hub/resources/utils.py
+++ mozichem_hub/resources/utils.py
@@ -82,3 +82,65 @@ def get_components_names(
     except Exception as e:
         logging.error(f"Failed to get component names: {e}")
         raise ValueError(f"Failed to get component names: {e}") from e
+
+
+def check_input(
+    value: str | int | float
+) -> int | float | str:
+    """
+    If value is a string that represents an int or float, convert and return the appropriate type.
+    Otherwise, return value as is.
+    """
+    try:
+        if isinstance(value, int) or isinstance(value, float):
+            return value
+        if isinstance(value, str):
+            value_stripped = value.strip()
+            # Try integer first
+            try:
+                return int(value_stripped)
+            except ValueError:
+                pass
+            # Try float
+            try:
+                return float(value_stripped)
+            except ValueError:
+                pass
+            # Return original string if not numeric
+            return value
+        return value
+    except Exception as e:
+        logging.error(f"Failed to detect digit/float from string: {e}")
+        raise ValueError(
+            f"Failed to detect digit/float from string: {e}") from e
+
+
+def convert_str_numeric_to_int(
+    value: str | int
+) -> int | str:
+    """
+    Convert a string that represents an integer or float to the appropriate type.
+    If the string is not numeric, return it as is.
+    """
+    try:
+        # NOTE: check if value is integer
+        if isinstance(value, int):
+            return value
+
+        # NOTE: check if value is string
+        if isinstance(value, str):
+            value_stripped = value.strip()
+            # Try integer first
+            try:
+                # check it has only digits
+                if value_stripped.isdigit():
+                    return int(value_stripped)
+                else:
+                    # return original string if not numeric
+                    return value
+            except ValueError:
+                pass
+        return value  # Return original value if not a string or not numeric
+    except Exception as e:
+        logging.error(f"Failed to convert string to numeric: {e}")
+        raise ValueError(f"Failed to convert string to numeric: {e}") from e
